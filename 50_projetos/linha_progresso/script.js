69b6024587b052d6d05a6bf67a147bd1e154a9e2
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-02-19 18:39:24 -0300

    projeto 1

--- 50_projetos/linha_progresso/script.js
+++ 50_projetos/linha_progresso/script.js
@@ -1,1 +1,5 @@
 
+const progresso = document.getElementById('progresso')
+const ante = document.getElementById('anterior')
+const prox = document.getElementById('proximo')
+const progresso = document.querry('.circle')
