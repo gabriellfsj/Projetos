761d52c42c66ce86a997ba9f86606829576d7829
Gabriel Francisco <76005679+gabriellfsj@users.noreply.github.com>
2021-02-21 18:38:25 -0300

    Progres steps

--- 50_projetos/linha_progresso/script.js
+++ 50_projetos/linha_progresso/script.js
@@ -2,4 +2,54 @@
 const progresso = document.getElementById('progresso')
 const ante = document.getElementById('anterior')
 const prox = document.getElementById('proximo')
-const progresso = document.querry('.circle')
+const circulo = document.querySelectorAll('.cprog')
+
+let currentActive = 1 
+
+prox.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circulo.length){
+        currentActive = circulo.length
+
+    }
+
+    update()
+
+})
+
+ante.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1){
+        currentActive = 1
+
+    }
+
+    update()
+
+})
+
+function update() {
+    circulo.forEach((cprog, idx) => {
+        if(idx < currentActive) {
+            cprog.classList.add('active')
+        } else {
+            cprog.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progresso.style.width = (actives.length - 1) / (circulo.length - 1) * 100 + '%'
+
+    if(currentActive === 1) {
+        ante.disabled = true
+    } else if(currentActive === circulo.length) {
+        prox.disabled = true
+    } else {
+        ante.disabled = false
+        prox.disabled = false
+    }
+}
+
